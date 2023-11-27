b67908ea6890d228c86a86fc73e6dd5a15ea9442
Krati Goyal <gkrati04@gmail.com>
2023-11-27 18:54:05 +0530

    Krati Goyal

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,4 +1,4 @@
-import React,{useEffect,useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import './Project.css'
 import LinkedGEMS from '../images/LinkedGEMS.png'
 import Leetcode from '../images/LeetcodeF.png'
@@ -19,221 +19,223 @@ const Project = () => {
                 WebkitTextFillColor: 'transparent',
                 fontFamily: "'Righteous', cursive",
             }}>Recent Work</h3>
-            <div id="container">
+            <div className='projectcards'>
+                <div id="container">
 
-                <div className="product-details">
+                    <div className="product-details">
 
-                    <h1>LinkedGEMS</h1>
-                    <span className="hint-star star">
-                        <i className="fa fa-star" aria-hidden="true"></i>
-                        <i className="fa fa-star" aria-hidden="true"></i>
-                        <i className="fa fa-star" aria-hidden="true"></i>
-                        <i className="fa fa-star" aria-hidden="true"></i>
-                        <i className="fa fa-star" aria-hidden="true"></i>
-                    </span>
+                        <h1>LinkedGEMS</h1>
+                        <span className="hint-star star">
+                            <i className="fa fa-star" aria-hidden="true"></i>
+                            <i className="fa fa-star" aria-hidden="true"></i>
+                            <i className="fa fa-star" aria-hidden="true"></i>
+                            <i className="fa fa-star" aria-hidden="true"></i>
+                            <i className="fa fa-star" aria-hidden="true"></i>
+                        </span>
 
-                    <p className="information">
-                        <ul>
-                            <li>The objective of this project is to create a platform that connects students with their
-                                respective alumnus who are placed at renowned MNCs. It provides the students with an insight of
-                                the placement related questions and feedbacks shared by the alumnus and their experiences at
-                                interviews. </li>
+                        <p className="information">
+                            <ul>
+                                <li>The objective of this project is to create a platform that connects students with their
+                                    respective alumnus who are placed at renowned MNCs. It provides the students with an insight of
+                                    the placement related questions and feedbacks shared by the alumnus and their experiences at
+                                    interviews. </li>
 
-                        </ul>
-                    </p>
+                            </ul>
+                        </p>
 
-                </div>
-
-
-
-                <div className="product-image">
-
-                    <img src={LinkedGEMS} alt="" />
+                    </div>
 
 
-                    <div className="info">
-                        <h2> Description</h2>
-                        <ul>
-                            <li><strong>Tech Stack : </strong> HTML | CSS | BootStrap | JavaScript | PHP | MySQL</li>
-                            <div className="link">
-                                <a href="https://github.com/iamkrati/LinkedGEMS/tree/master" target="_blank"><i
-                                    className="fa-brands fa-github"></i>View Project</a>
-                            </div>
-                        </ul>
+
+                    <div className="product-image">
+
+                        <img src={LinkedGEMS} alt="" />
+
+
+                        <div className="info">
+                            <h2> Description</h2>
+                            <ul>
+                                <li><strong>Tech Stack : </strong> HTML | CSS | BootStrap | JavaScript | PHP | MySQL</li>
+                                <div className="link">
+                                    <a href="https://github.com/iamkrati/LinkedGEMS/tree/master" target="_blank"><i
+                                        className="fa-brands fa-github"></i>View Project</a>
+                                </div>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div id="container">
+                <div id="container">
 
-                <div className="product-details">
+                    <div className="product-details">
 
-                    <h1>Expense Tracker</h1>
-                    <span className="hint-star star">
-                        <i className="fa fa-star" aria-hidden="true"></i>
-                        <i className="fa fa-star" aria-hidden="true"></i>
-                        <i className="fa fa-star" aria-hidden="true"></i>
-                        <i className="fa fa-star" aria-hidden="true"></i>
-                        <i className="fa fa-star" aria-hidden="true"></i>
-                    </span>
+                        <h1>Expense Tracker</h1>
+                        <span className="hint-star star">
+                            <i className="fa fa-star" aria-hidden="true"></i>
+                            <i className="fa fa-star" aria-hidden="true"></i>
+                            <i className="fa fa-star" aria-hidden="true"></i>
+                            <i className="fa fa-star" aria-hidden="true"></i>
+                            <i className="fa fa-star" aria-hidden="true"></i>
+                        </span>
 
-                    <p className="information">
-                        <ul>
-                            <li>Build an Expense Tracker Application. It uses functional components with hooks and the context API.
-                            </li>
-                            <li>We can Add new transactions and reflect them in total , delete items from DOM and persist it MongoDB Database.</li>
-                        </ul>
-                    </p>
+                        <p className="information">
+                            <ul>
+                                <li>Build an Expense Tracker Application. It uses functional components with hooks and the context API.
+                                </li>
+                                <li>We can Add new transactions and reflect them in total , delete items from DOM and persist it MongoDB Database.</li>
+                            </ul>
+                        </p>
 
-                </div>
-
+                    </div>
 
 
-                <div className="product-image">
 
-                    <img src={Expense} alt="" />
+                    <div className="product-image">
+
+                        <img src={Expense} alt="" />
 
 
-                    <div className="info">
-                        <h2> Description</h2>
-                        <ul>
-                            <li><strong>Tech Stack : </strong> HTML | CSS | JavaScript | React | MongoDB | Expess JS | Node JS</li>
-                            <div className="link">
-                                <a href="https://expense-tracker-ugzs.onrender.com/" target="_blank"><i
-                                    className="fa-brands fa-github"></i>View Project</a>
-                            </div>
-                        </ul>
+                        <div className="info">
+                            <h2> Description</h2>
+                            <ul>
+                                <li><strong>Tech Stack : </strong> HTML | CSS | JavaScript | React | MongoDB | Expess JS | Node JS</li>
+                                <div className="link">
+                                    <a href="https://expense-tracker-ugzs.onrender.com/" target="_blank"><i
+                                        className="fa-brands fa-github"></i>View Project</a>
+                                </div>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div id="container">
+                <div id="container">
 
-                <div className="product-details">
+                    <div className="product-details">
 
-                    <h1>LeetCode Filter</h1>
-                    <span className="hint-star star">
-                        <i className="fa fa-star" aria-hidden="true"></i>
-                        <i className="fa fa-star" aria-hidden="true"></i>
-                        <i className="fa fa-star" aria-hidden="true"></i>
-                        <i className="fa fa-star" aria-hidden="true"></i>
-                        <i className="fa fa-star" aria-hidden="true"></i>
-                    </span>
+                        <h1>LeetCode Filter</h1>
+                        <span className="hint-star star">
+                            <i className="fa fa-star" aria-hidden="true"></i>
+                            <i className="fa fa-star" aria-hidden="true"></i>
+                            <i className="fa fa-star" aria-hidden="true"></i>
+                            <i className="fa fa-star" aria-hidden="true"></i>
+                            <i className="fa fa-star" aria-hidden="true"></i>
+                        </span>
 
-                    <p className="information">
-                        <ul>
-                            <li>Developed a LeetCode Filter to solve a questions on LeetCode.</li>
-                            <li>The configuration includes various user activities on a webpage, such as user’s dashboard ,
-                                choose a
-                                topic and question
-                                difficulty level.</li>
-                            <li>Added Chrome Extention Feature which is very effective.
-                            </li>
-                        </ul>
-                    </p>
+                        <p className="information">
+                            <ul>
+                                <li>Developed a LeetCode Filter to solve a questions on LeetCode.</li>
+                                <li>The configuration includes various user activities on a webpage, such as user’s dashboard ,
+                                    choose a
+                                    topic and question
+                                    difficulty level.</li>
+                                <li>Added Chrome Extention Feature which is very effective.
+                                </li>
+                            </ul>
+                        </p>
 
-                </div>
-                <div className="product-image">
+                    </div>
+                    <div className="product-image">
 
-                    <img src={Leetcode} alt="" />
+                        <img src={Leetcode} alt="" />
 
 
-                    <div className="info">
-                        <h2> Description</h2>
-                        <ul>
-                            <li><strong>Tech Stack : </strong> HTML | CSS | JavaScript | React | MongoDB | Expess JS | Node JS </li>
-                            <div className="link">
-                                <a href="https://github.com/iamkrati/LeetCode-Filter" target="_blank"><i
-                                    className="fa-brands fa-github"></i>View Project</a>
-                            </div>
-                        </ul>
+                        <div className="info">
+                            <h2> Description</h2>
+                            <ul>
+                                <li><strong>Tech Stack : </strong> HTML | CSS | JavaScript | React | MongoDB | Expess JS | Node JS </li>
+                                <div className="link">
+                                    <a href="https://github.com/iamkrati/LeetCode-Filter" target="_blank"><i
+                                        className="fa-brands fa-github"></i>View Project</a>
+                                </div>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div id="container">
+                <div id="container">
 
-                <div className="product-details">
+                    <div className="product-details">
 
-                    <h1>E-Commerce Website</h1>
-                    <span className="hint-star star">
-                        <i className="fa fa-star" aria-hidden="true"></i>
-                        <i className="fa fa-star" aria-hidden="true"></i>
-                        <i className="fa fa-star" aria-hidden="true"></i>
-                        <i className="fa fa-star" aria-hidden="true"></i>
-                        <i className="fa fa-star" aria-hidden="true"></i>
-                    </span>
+                        <h1>E-Commerce Website</h1>
+                        <span className="hint-star star">
+                            <i className="fa fa-star" aria-hidden="true"></i>
+                            <i className="fa fa-star" aria-hidden="true"></i>
+                            <i className="fa fa-star" aria-hidden="true"></i>
+                            <i className="fa fa-star" aria-hidden="true"></i>
+                            <i className="fa fa-star" aria-hidden="true"></i>
+                        </span>
 
-                    <p className="information">
-                        <ul>
-                            <li>Developed a website with higher security using the passport module.</li>
-                            <li>Here we can add/delete products and there is also a review section.
-                            </li>
-                            <li>There are two sections of Retailer and Customer.
-                            </li>
-                        </ul>
-                    </p>
+                        <p className="information">
+                            <ul>
+                                <li>Developed a website with higher security using the passport module.</li>
+                                <li>Here we can add/delete products and there is also a review section.
+                                </li>
+                                <li>There are two sections of Retailer and Customer.
+                                </li>
+                            </ul>
+                        </p>
 
-                </div>
+                    </div>
 
-                <div className="product-image">
+                    <div className="product-image">
 
-                    <img src={ecommerce} alt="" />
+                        <img src={ecommerce} alt="" />
 
 
-                    <div className="info">
-                        <h2> Description</h2>
-                        <ul>
-                            <li><strong>Tech Stack : </strong> HTML | CSS | BootStrap | JavaScript | LeetCode API</li>
-                            <div className="link">
-                                <a href="https://ecomm-omnq.onrender.com/" target="_blank"><i
-                                    className="fa-brands fa-github"></i>View Project</a>
-                            </div>
-                        </ul>
+                        <div className="info">
+                            <h2> Description</h2>
+                            <ul>
+                                <li><strong>Tech Stack : </strong> HTML | CSS | BootStrap | JavaScript | LeetCode API</li>
+                                <div className="link">
+                                    <a href="https://ecomm-omnq.onrender.com/" target="_blank"><i
+                                        className="fa-brands fa-github"></i>View Project</a>
+                                </div>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div id="container">
+                <div id="container">
 
-                <div className="product-details">
+                    <div className="product-details">
 
-                    <h1>MyPortfolio</h1>
-                    <span className="hint-star star">
-                        <i className="fa fa-star" aria-hidden="true"></i>
-                        <i className="fa fa-star" aria-hidden="true"></i>
-                        <i className="fa fa-star" aria-hidden="true"></i>
-                        <i className="fa fa-star" aria-hidden="true"></i>
-                        <i className="fa fa-star" aria-hidden="true"></i>
-                    </span>
+                        <h1>MyPortfolio</h1>
+                        <span className="hint-star star">
+                            <i className="fa fa-star" aria-hidden="true"></i>
+                            <i className="fa fa-star" aria-hidden="true"></i>
+                            <i className="fa fa-star" aria-hidden="true"></i>
+                            <i className="fa fa-star" aria-hidden="true"></i>
+                            <i className="fa fa-star" aria-hidden="true"></i>
+                        </span>
 
-                    <p className="information">
-                        <ul>
-                            <li>Developed a interactive web application.</li>
-                            <li>Added chat support using a TIDIO live chat and also Leetcode Stat's.</li>
-                            <li>It contains all the information related to my education,experience,skills and my all social
-                                media
-                                links.
-                            </li>
-                        </ul>
-                    </p>
+                        <p className="information">
+                            <ul>
+                                <li>Developed a interactive web application.</li>
+                                <li>Added chat support using a TIDIO live chat and also Leetcode Stat's.</li>
+                                <li>It contains all the information related to my education,experience,skills and my all social
+                                    media
+                                    links.
+                                </li>
+                            </ul>
+                        </p>
 
-                </div>
+                    </div>
 
-                <div className="product-image">
+                    <div className="product-image">
 
-                    <img src={portfolio} alt="" />
+                        <img src={portfolio} alt="" />
 
 
-                    <div className="info">
-                        <h2> Description</h2>
-                        <ul>
-                            <li><strong>Tech Stack : </strong> HTML | CSS | BootStrap | JavaScript | LeetCode API</li>
-                            <div className="link">
-                                <a href="https://iamkrati.github.io/Krati-sPortfolio/index.html" target="_blank"><i
-                                    className="fa-brands fa-github"></i>View Project</a>
-                            </div>
-                        </ul>
+                        <div className="info">
+                            <h2> Description</h2>
+                            <ul>
+                                <li><strong>Tech Stack : </strong> HTML | CSS | BootStrap | JavaScript | LeetCode API</li>
+                                <div className="link">
+                                    <a href="https://iamkrati.github.io/Krati-sPortfolio/index.html" target="_blank"><i
+                                        className="fa-brands fa-github"></i>View Project</a>
+                                </div>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
